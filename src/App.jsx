4725ec0eae4dcf8f15da4d745b931a29f890c5ef
unknown <bashawdejenu1221@gmail.com>
2025-01-10 11:05:57 +0300

    footer, appdownload and make responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -14,7 +16,11 @@ const App = () => {
         <Route path='/' element={<Cart/>}/>
         <Route path='/' element={<PlaceOrder/>}/>
       </Routes>
+      
     </div>
+    <Footer/>
+    </>
+    
   )
 }
 
--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,9 +9,11 @@ const FoodDisplay = ({category}) => {
      <h2>Top dishes near you</h2>
      <div className="food-display-list">
         {food_list.map((item,index) => {
-          return(
-            <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
-          )
+          if (category==='All' || category===item.category) {
+            return(
+              <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
+            )
+          }
         })}
       </div>
       </div>
