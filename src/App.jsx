2770ab550af96a308c78c4bac3700202748dfbc2
unknown <bashawdejenu1221@gmail.com>
2025-01-11 15:43:55 +0300

    updating the cart page

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path="/cart" element={<Cart />}/>
+        <Route path='/cart' element={<Cart />}/>
         <Route path='/' element={<PlaceOrder/>}/>
       </Routes>
       
