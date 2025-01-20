e35be61bd23df4b4180cd1d715fce8aa460be6af
unknown <bashawdejenu1221@gmail.com>
2025-01-20 22:21:07 +0300

    update the backend file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
-import mongoose from "mongoose "
+import mongoose from 'mongoose'; 
 
-const connectDB =  async () => {
-  await mongoose.connect('mongodb+srv://bashawdejenu1221:bd21211212@cluster0.wopwb.mongodb.net/?')
+
+export const connectDB =  async () => {
+  await mongoose.connect('mongodb+srv://bashawdejenu1221:bd21211212@cluster0.wopwb.mongodb.net/food-del').then(() => console.log("DB Connected"))
 }
+
